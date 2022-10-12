e4fadcda41bf711be973d9f4b61e0d12a8498c33
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-10-12 12:39:02 +0100

    creates Form

---
+++ src/components/Contacts/Form.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Contacts from "./Contacts";
+import { useDispatch, useSelector } from "react-redux";
+import { addContact } from "redux/reducer";
+import { getContacts } from "redux/selectors";
+
+const Form = () => {
+    const dispatch = useDispatch();
+    const numbers = useSelector(getContacts);
+    const onAddContactToState = e => {
+      e.preventDefault();
+      const form = e.target;
+      if (
+        numbers.some(
+          contact =>
+            contact.name.toLocaleLowerCase() ===
+            form.elements.name.value.toLocaleLowerCase()
+        )
+      ) {
+        window.alert(form.elements.name.value + 'is already in contacts');
+        return;
+      }
+      dispatch(addContact(form.elements.name.value, form.elements.number.value));
+      form.reset();
+    };
+  
+    return (
+      <>
+        <h1>PhoneBook</h1>
+        <form action="#" onSubmit={onAddContactToState}>
+          <label htmlFor="addContactName">Name</label>
+          <input
+            id="addContactName"
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+          <button type="submit">Add contact</button>
+        </form>
+        <Contacts />
+      </>
+    );
+  };
+  export default Form;
